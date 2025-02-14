e78c67f7d4a255fdb723c878f6f608e9c571e4c4
ayaatef11 <ayabadrin667@gmail.com>
2025-02-14 21:01:39 +0200

    create header page

---
+++ src/app/desing/desing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desing',
+  standalone: true,
+  imports: [],
+  templateUrl: './desing.component.html',
+  styleUrl: './desing.component.css'
+})
+export class DesingComponent {
+
+}
