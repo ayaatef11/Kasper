a6780377438733f89ef0437fe077317120162c11
ayaatef11 <ayabadrin667@gmail.com>
2025-02-15 10:58:39 +0200

    add features to the design page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  socialIcons = [
+    { class: 'fab fa-facebook-f', link: 'https://www.facebook.com' },
+    { class: 'fab fa-twitter', link: 'https://www.twitter.com' },
+    { class: 'fas fa-home', link: '/' },
+    { class: 'fab fa-linkedin', link: 'https://www.linkedin.com' }
+  ];
 }
