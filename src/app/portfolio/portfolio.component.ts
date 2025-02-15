c3a211778a947af6bb3efd9259c7316a4559896f
ayaatef11 <ayabadrin667@gmail.com>
2025-02-15 10:08:58 +0200

    fix path error which ws the double cautation

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-portfolio',
@@ -9,16 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
-  categories = ['All', 'App', 'Photo', 'Web', 'Print'];
-
+ categories = ['All', 'App', 'Photo', 'Web', 'Print'];
+path="/images/shuffle-01.jpg"
+//src must start with / and the path is between  "" not '' ok )
   images = [
-    { src: 'assets/images/image1.jpg', title: 'Awesome Image', category: 'Photography' },
-    { src: 'assets/images/image2.jpg', title: 'Awesome Image', category: 'Photography' },
-    { src: 'assets/images/image3.jpg', title: 'Awesome Image', category: 'Photography' },
-    { src: 'assets/images/image4.jpg', title: 'Awesome Image', category: 'Photography' },
-    { src: 'assets/images/image5.jpg', title: 'Awesome Image', category: 'Photography' },
-    { src: 'assets/images/image6.jpg', title: 'Awesome Image', category: 'Photography' },
-    { src: 'assets/images/image7.jpg', title: 'Awesome Image', category: 'Photography' },
-    { src: 'assets/images/image8.jpg', title: 'Awesome Image', category: 'Photography' }
+    { src: "/images/shuffle-01.jpg", title: 'Awesome Image', category: 'Photography' },
+    { src: "/images/shuffle-02.jpg", title: 'Awesome Image', category: 'Photography' },
+    { src: "/images/shuffle-03.jpg", title: 'Awesome Image', category: 'Photography' },
+    { src: "/images/shuffle-04.jpg", title: 'Awesome Image', category: 'Photography' },
+    { src: "/images/shuffle-05.jpg", title: 'Awesome Image', category: 'Photography' },
+    { src: "/images/shuffle-06.jpg", title: 'Awesome Image', category: 'Photography' },
+    { src: "/images/shuffle-07.jpg", title: 'Awesome Image', category: 'Photography' },
+    { src: "/images/shuffle-08.jpg", title: 'Awesome Image', category: 'Photography' }
   ];
 }
