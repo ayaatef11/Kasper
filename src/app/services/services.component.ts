b9da4bcc1622d3f6f7c466cacfdbaa213ace1283
ayaatef11 <ayabadrin667@gmail.com>
2025-02-15 11:14:23 +0200

    update the services page to use for loop

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,12 +1,19 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
+  services = [
+    { icon: 'fas fa-desktop fa-3x', title: 'Vorem amet intuitive', description: 'Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam.' },
+    { icon: 'fas fa-cog fa-3x', title: 'Vorem amet intuitive', description: 'Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam.' },
+    { icon: 'fas fa-pencil-ruler fa-3x', title: 'Vorem amet intuitive', description: 'Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam.' },
+    { icon: 'fas fa-camera fa-3x', title: 'Vorem amet intuitive', description: 'Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam.' }
+  ];
 
 }
