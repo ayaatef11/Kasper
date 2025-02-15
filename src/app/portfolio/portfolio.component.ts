f1d6e8ebdfa708b88c8be516fcfee782ee294ff9
ayaatef11 <ayabadrin667@gmail.com>
2025-02-15 08:08:20 +0200

    add image paths

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
-
+path1="/public/images/shuffle-01.jpg";
+path2="/public/images/shuffle-02.jpg";
+path3="/public/images/shuffle-03.jpg";
+path4="/public/images/shuffle-04.jpg";
+path5="/public/images/shuffle-05.jpg"
+path6="/public/images/shuffle-06.jpg";
+path7="/public/images/shuffle-07.jpg";
+path8="/public/images/shuffle-08.jpg";
 }
